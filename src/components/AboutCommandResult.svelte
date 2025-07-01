7c04f9505b2b78760bb0c1ea2883b3601d4de84c
aasmart <alexandersmart2005@gmail.com>
2025-07-01 11:23:18 -0400

    About resizing + content

--- src/components/AboutCommandResult.svelte
+++ src/components/AboutCommandResult.svelte
@@ -12,19 +12,28 @@
 
 <section
 	use:focus
-	class="flex w-max max-w-full animate-grow-fade-in flex-col gap-4 rounded-md border-2 border-blue-400 p-2 sm:flex-row"
+	class="flex max-w-[100rem] animate-grow-fade-in flex-col items-center gap-4 rounded-md border-2 border-blue-400 p-2 sm:w-3/4 lg:flex-row"
 >
 	{focusCommand()}
-	<img class="aspect-square max-w-[16em] rounded-md shadow-lg" src={'/me.png'} alt="Me" />
+	<img
+		class="aspect-square max-h-[18em] rounded-full shadow-lg sm:w-auto"
+		src={'/me.png'}
+		alt="Me"
+	/>
 	<div class="flex h-auto flex-col dark:text-gray-50">
-		<h2 class="text-xl font-bold">Alexander Smart</h2>
-		<h3 class="text-lg dark:text-gray-100">Software Engineer</h3>
+		<h2 class="text-center text-xl font-bold lg:text-start">Alexander Smart</h2>
+		<h3 class="text-center text-lg lg:text-start dark:text-gray-100">Student and Developer</h3>
 		<hr class="border-1 my-1 rounded-full" />
-		<p>
-			Hi! <span>👋</span> I'm currently an undergraduate student at the University of Michigan -- Ann
-			Arbor.
+		<p class="h-max w-full text-center lg:text-start">
+			Hi! <span>👋</span> I'm currently an undergraduate student at the University of Michigan
+			&mdash; Ann Arbor. I'm pretty interested in most things computer science, but I've discovered
+			that I especially enjoy programming languages, systems, and building useful software.
+
+			<br /><br />
+			This site is still in its early days, so if it seems a little empty here, it's because it is. However,
+			I invite you to have a look around with either the nav buttons, or with the actual command line.
 		</p>
-		<ul class="mt-auto flex flex-row gap-3 pt-2 text-4xl">
+		<ul class="mt-auto flex flex-row justify-center gap-3 pt-2 text-4xl lg:justify-start">
 			<li>
 				<a
 					class="text-blue-400 transition-colors duration-200 hover:text-blue-500 focus:text-blue-500"
