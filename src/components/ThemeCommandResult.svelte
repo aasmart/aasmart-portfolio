eb7695f7ea64cf8803357ba74d4d424ade4d34b1
aasmart <alexandersmart2005@gmail.com>
2025-07-02 14:41:17 -0400

    Style theme picker

--- src/components/ThemeCommandResult.svelte
+++ src/components/ThemeCommandResult.svelte
@@ -24,15 +24,24 @@
 			setTheme('light');
 		}
 	}
-
-	//TODO: case when no experiences
 </script>
 
 <section use:focus class="text-gray-900 motion-safe:animate-grow-fade-in dark:text-gray-50">
-	<h2 class="w-full items-center text-xl">
+	<h2 class="flex w-full flex-row items-center gap-2 text-xl">
 		<span class="font-bold text-blue-400">export</span>
 		<span>THEME</span>
 		<span class="font-bold text-blue-400">=</span>
-		<button onclick={toggleTheme}>{theme}</button>
+		<button
+			class="group relative aspect-square overflow-hidden rounded-md border-2 border-blue-400 p-2"
+			onclick={toggleTheme}
+			aria-label="Toggle theme"
+		>
+			<span
+				class="inline-block aspect-square h-8 w-8 rounded-full bg-amber-400 shadow-[0_0_2px_2px] shadow-amber-400 transition-all group-hover:bg-blue-200 group-hover:shadow-blue-200 dark:bg-blue-200 dark:shadow-blue-200 dark:group-hover:bg-amber-400 dark:group-hover:shadow-amber-400"
+			></span>
+			<span
+				class="absolute -right-7 -top-3 aspect-square h-8 w-8 rounded-full bg-light-bg shadow-[0_0_2px_2px] shadow-light-bg/30 transition-[top,right] group-hover:-right-[2px] group-hover:top-[3px] dark:-right-[2px] dark:top-[3px] dark:bg-gray-bg dark:shadow-gray-bg/30 dark:group-hover:-right-7 dark:group-hover:-top-3"
+			></span>
+		</button>
 	</h2>
 </section>
