61b105bc23db848fb2846265b6c6a1c61365fa8d
aasmart <alexandersmart2005@gmail.com>
2025-07-07 22:34:45 -0400

    Fix projects route 404

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readdirSync } from 'fs'
+
+// make it so project routes work
+const files = readdirSync('static/projects/');
+let file_routes = files.filter(f => f.endsWith("md")).map(f => `/projects/${f.slice(0, -3)}/`);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,10 +18,13 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '' : ''
+      base: process.env.NODE_ENV === 'production' ? '/aasmart-portfolio' : ''
     },
     prerender: {
-      entries: [`/projects/[slug]`, '/']
+      entries: [
+        ...file_routes,
+        '/'
+      ]
     }
   }
 };
